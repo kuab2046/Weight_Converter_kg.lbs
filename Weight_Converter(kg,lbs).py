a8aa45c2ff1f7ac723b86303292bd5cc7e5914b5
kuab2046 <olejniczakk10@gmail.com>
2026-06-01 15:39:54 +0200

    I have added rounding the result, because I didn't want the result to be so long

--- Weight_Converter(kg,lbs).py
+++ Weight_Converter(kg,lbs).py
@@ -18,7 +18,7 @@ if kind_of_change == 1:
         print("it is impossible for mass to be negative")
         exit(0)
     result = amount * kg_to_lbs
-    print(f"after converting {amount} kg to pounds your result is {result} lbs")
+    print(f"after converting {amount} kg to pounds your result is {round(result, 2)} lbs")
 elif kind_of_change == 2:
     amount = input("how many lbs would you like to convert into kilograms?: ")
     try:
@@ -30,4 +30,4 @@ elif kind_of_change == 2:
         print("it is impossible for mass to be negative")
         exit(0)
     result = amount * lbs_to_kg
-    print(f"after converting {amount} pounds to kg your result is {result} kg")
+    print(f"after converting {amount} pounds to kg your result is {round(result, 2)} kg")
